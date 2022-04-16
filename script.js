972d90f725c76d5ac16826a5bc9ba5901c9cd856
Vomitatrix <david.j.procop@gmail.com>
2022-04-15 23:32:42 -0400

    Added all variables to script

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button');
+let ingredientOne = document.querySelector('#ingredientOne');
+let ingredientTwo = document.querySelector('#ingredientTwo');
+let ingredientThree = document.querySelector('#ingredientThree');
+let ingredientFour = document.querySelector('#ingredientFour');
+
+const protein = ['Turkey', 'Tuna', 'Pastrami', 'Chicken', 'Crab', 'Egg salad'];
+const veggie = ['Lettuce', 'Tomato', 'Avocado', 'Onion', 'Apple', 'Pickle'] ;
+const other = ['Cheese', 'Dill', 'Salt and pepper', 'rollSauce', 'rollVeggie', 'rollProtein'];
+const sauce =['Mayo', 'Ketchup', 'Red wine vinegar', 'Mustard', 'Peanut butter', 'Chick-fil-A'];
